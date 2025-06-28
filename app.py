b1595782e523ec951571949efd76b7cc0c0fb9a8
kudiyarohit <2022csrohit12759@poornima.edu.in>
2025-06-28 18:00:23 +0530

    Port Fix

--- app.py
+++ app.py
@@ -40,4 +40,6 @@ for subfolder in ['uploads', 'uploads/questions', 'uploads/answers', 'uploads/ke
     os.makedirs(subfolder, exist_ok=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
